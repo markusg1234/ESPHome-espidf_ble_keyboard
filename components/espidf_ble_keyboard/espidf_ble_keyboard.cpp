cf662362a26eeb737a11e271d039ac9eabad108d
markus1234 <2220900+markusg1234@users.noreply.github.com>
2026-02-24 22:50:15 +1030

    Refactor BLE keyboard GATT attribute handling and improve connection ID management for better stability

--- components/espidf_ble_keyboard/espidf_ble_keyboard.cpp
+++ components/espidf_ble_keyboard/espidf_ble_keyboard.cpp
@@ -20,10 +20,9 @@ namespace espidf_ble_keyboard {
 static const char *TAG = "espidf_ble_keyboard";
 static EspidfBleKeyboard *s_instance = nullptr;
 
-// ── HID Report Descriptor (The "Map" Windows needs to see) ──
 static const uint8_t hid_report_map[] = {
     0x05, 0x01, 0x09, 0x06, 0xA1, 0x01,
-    0x85, 0x01,  // Report ID 1
+    0x85, 0x01,
     0x05, 0x07, 0x19, 0xE0, 0x29, 0xE7,
     0x15, 0x00, 0x25, 0x01, 0x75, 0x01, 0x95, 0x08, 0x81, 0x02,
     0x95, 0x01, 0x75, 0x08, 0x81, 0x01,
@@ -34,10 +33,8 @@ static const uint8_t hid_report_map[] = {
     0xC0
 };
 
-#define GATTS_APP_ID    0x55
-#define HID_NUM_HANDLES 22
-
 static esp_gatt_if_t s_gatts_if = ESP_GATT_IF_NONE;
+static uint16_t s_conn_id = 0;
 static uint16_t s_hid_report_handle = 0;
 
 static esp_ble_adv_params_t adv_params = {
@@ -49,17 +46,7 @@ static esp_ble_adv_params_t adv_params = {
     .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
 
-static uint8_t adv_data_raw[] = {
-    0x02, 0x01, 0x06,           
-    0x03, 0x19, 0xC1, 0x03,     
-    0x03, 0x03, 0x12, 0x18,     
-};
-
-static uint8_t scan_rsp_raw[] = {
-    0x11, 0x09, 'E','S','P','3','2',' ','K','e','y','b','o','a','r','d'
-};
-
-// ── GATT Attribute Table (Essential for Windows Connection) ──
+// ── Attributes ──
 enum {
     IDX_SVC,
     IDX_CHAR_HID_INFO,     IDX_CHAR_HID_INFO_VAL,
@@ -73,43 +60,27 @@ enum {
 };
 
 static uint16_t hid_handle_table[HID_IDX_NB];
-static uint8_t  hid_info_val[4]   = {0x11, 0x01, 0x00, 0x01};
-static uint8_t  hid_ctrl_val      = 0;
-static uint8_t  proto_mode_val    = 0x01;
-static uint8_t  report_val[8]     = {0};
-static uint16_t report_ccc_val    = 0;
-static uint8_t  report_ref_val[2] = {0x01, 0x01};
-
-static const uint16_t UUID_PRI_SERVICE        = ESP_GATT_UUID_PRI_SERVICE;
-static const uint16_t UUID_HID_SVC            = ESP_GATT_UUID_HID_SVC;
-static const uint16_t UUID_CHAR_DECLARE       = ESP_GATT_UUID_CHAR_DECLARE;
-static const uint16_t UUID_HID_INFORMATION    = ESP_GATT_UUID_HID_INFORMATION;
-static const uint16_t UUID_HID_REPORT_MAP     = ESP_GATT_UUID_HID_REPORT_MAP;
-static const uint16_t UUID_HID_CONTROL_POINT  = ESP_GATT_UUID_HID_CONTROL_POINT;
-static const uint16_t UUID_HID_PROTO_MODE     = ESP_GATT_UUID_HID_PROTO_MODE;
-static const uint16_t UUID_HID_REPORT         = ESP_GATT_UUID_HID_REPORT;
-static const uint16_t UUID_CHAR_CLIENT_CONFIG = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
-static const uint16_t UUID_RPT_REF_DESCR      = ESP_GATT_UUID_RPT_REF_DESCR;
-
-static const uint8_t PROP_READ        = ESP_GATT_CHAR_PROP_BIT_READ;
-static const uint8_t PROP_WRITE_NR    = ESP_GATT_CHAR_PROP_BIT_WRITE_NR;
-static const uint8_t PROP_RW_NR       = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE_NR;
-static const uint8_t PROP_READ_NOTIFY = ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+static uint8_t hid_info_val[4] = {0x11, 0x01, 0x00, 0x01};
+static uint8_t hid_ctrl_val = 0;
+static uint8_t proto_mode_val = 0x01;
+static uint8_t report_val[8] = {0};
+static uint16_t report_ccc_val = 0;
+static uint8_t report_ref_val[2] = {0x01, 0x01};
 
 static const esp_gatts_attr_db_t hid_attr_db[HID_IDX_NB] = {
-    [IDX_SVC] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_PRI_SERVICE, ESP_GATT_PERM_READ, 2, 2, (uint8_t *)&UUID_HID_SVC}},
-    [IDX_CHAR_HID_INFO] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_DECLARE, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&PROP_READ}},
-    [IDX_CHAR_HID_INFO_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_HID_INFORMATION, ESP_GATT_PERM_READ, 4, 4, hid_info_val}},
-    [IDX_CHAR_REPORT_MAP] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_DECLARE, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&PROP_READ}},
-    [IDX_CHAR_REPORT_MAP_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_HID_REPORT_MAP, ESP_GATT_PERM_READ, sizeof(hid_report_map), sizeof(hid_report_map), (uint8_t *)hid_report_map}},
-    [IDX_CHAR_HID_CTRL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_DECLARE, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&PROP_WRITE_NR}},
-    [IDX_CHAR_HID_CTRL_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_HID_CONTROL_POINT, ESP_GATT_PERM_WRITE, 1, 1, &hid_ctrl_val}},
-    [IDX_CHAR_PROTO_MODE] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_DECLARE, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&PROP_RW_NR}},
-    [IDX_CHAR_PROTO_MODE_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_HID_PROTO_MODE, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, 1, 1, &proto_mode_val}},
-    [IDX_CHAR_REPORT] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_DECLARE, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&PROP_READ_NOTIFY}},
-    [IDX_CHAR_REPORT_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_HID_REPORT, ESP_GATT_PERM_READ, 8, 8, report_val}},
-    [IDX_CHAR_REPORT_CCC] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_CHAR_CLIENT_CONFIG, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, 2, 2, (uint8_t *)&report_ccc_val}},
-    [IDX_CHAR_REPORT_REF] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&UUID_RPT_REF_DESCR, ESP_GATT_PERM_READ, 2, 2, report_ref_val}},
+    [IDX_SVC] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_PRI_SERVICE}, ESP_GATT_PERM_READ, 2, 2, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_SVC}}},
+    [IDX_CHAR_HID_INFO] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_DECLARE}, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&(uint8_t){ESP_GATT_CHAR_PROP_BIT_READ}}},
+    [IDX_CHAR_HID_INFO_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_INFORMATION}, ESP_GATT_PERM_READ, 4, 4, hid_info_val}},
+    [IDX_CHAR_REPORT_MAP] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_DECLARE}, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&(uint8_t){ESP_GATT_CHAR_PROP_BIT_READ}}},
+    [IDX_CHAR_REPORT_MAP_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_REPORT_MAP}, ESP_GATT_PERM_READ, sizeof(hid_report_map), sizeof(hid_report_map), (uint8_t *)hid_report_map}},
+    [IDX_CHAR_HID_CTRL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_DECLARE}, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&(uint8_t){ESP_GATT_CHAR_PROP_BIT_WRITE_NR}}},
+    [IDX_CHAR_HID_CTRL_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_CONTROL_POINT}, ESP_GATT_PERM_WRITE, 1, 1, &hid_ctrl_val}},
+    [IDX_CHAR_PROTO_MODE] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_DECLARE}, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&(uint8_t){ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_WRITE_NR}}},
+    [IDX_CHAR_PROTO_MODE_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_PROTO_MODE}, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, 1, 1, &proto_mode_val}},
+    [IDX_CHAR_REPORT] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_DECLARE}, ESP_GATT_PERM_READ, 1, 1, (uint8_t *)&(uint8_t){ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY}}},
+    [IDX_CHAR_REPORT_VAL] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_HID_REPORT}, ESP_GATT_PERM_READ, 8, 8, report_val}},
+    [IDX_CHAR_REPORT_CCC] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_CHAR_CLIENT_CONFIG}, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE, 2, 2, (uint8_t *)&report_ccc_val}},
+    [IDX_CHAR_REPORT_REF] = {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&(uint16_t){ESP_GATT_UUID_RPT_REF_DESCR}, ESP_GATT_PERM_READ, 2, 2, report_ref_val}},
 };
 
 static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
@@ -125,12 +96,11 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
             esp_ble_gatts_start_service(hid_handle_table[IDX_SVC]);
             break;
         case ESP_GATTS_START_EVT:
-            esp_ble_gap_config_adv_data_raw(adv_data_raw, sizeof(adv_data_raw));
-            esp_ble_gap_config_scan_rsp_data_raw(scan_rsp_raw, sizeof(scan_rsp_raw));
             esp_ble_gap_start_advertising(&adv_params);
             break;
         case ESP_GATTS_CONNECT_EVT:
-            if (s_instance) s_instance->set_connected(true, param->connect.conn_id);
+            s_conn_id = param->connect.conn_id;
+            if (s_instance) s_instance->set_connected(true, s_conn_id);
             break;
         case ESP_GATTS_DISCONNECT_EVT:
             if (s_instance) s_instance->set_connected(false, 0);
@@ -150,7 +120,7 @@ void EspidfBleKeyboard::setup() {
     esp_bluedroid_init();
     esp_bluedroid_enable();
     esp_ble_gatts_register_callback(gatts_event_handler);
-    esp_ble_gatts_app_register(GATTS_APP_ID);
+    esp_ble_gatts_app_register(0x55);
 }
 
 void EspidfBleKeyboard::loop() {}
@@ -160,11 +130,10 @@ void EspidfBleKeyboard::send_key_combo(uint8_t modifiers, uint8_t keycode) {
     uint8_t report[8] = {0};
     report[0] = modifiers;
     report[2] = keycode;
-    esp_ble_gatts_send_indicate(s_gatts_if, conn_id_, s_hid_report_handle, 8, report, false);
-    vTaskDelay(pdMS_TO_TICKS(50));
-    memset(report, 0, 8);
-    esp_ble_gatts_send_indicate(s_gatts_if, conn_id_, s_hid_report_handle, 8, report, false);
+    esp_ble_gatts_send_indicate(s_gatts_if, s_conn_id, s_hid_report_handle, 8, report, false);
     vTaskDelay(pdMS_TO_TICKS(20));
+    memset(report, 0, 8);
+    esp_ble_gatts_send_indicate(s_gatts_if, s_conn_id, s_hid_report_handle, 8, report, false);
 }
 
 void EspidfBleKeyboard::send_string(const std::string &str) {
@@ -177,10 +146,10 @@ void EspidfBleKeyboard::send_string(const std::string &str) {
         else if (c == '\n') report[2] = 0x28;
         else continue;
 
-        esp_ble_gatts_send_indicate(s_gatts_if, conn_id_, s_hid_report_handle, 8, report, false);
+        esp_ble_gatts_send_indicate(s_gatts_if, s_conn_id, s_hid_report_handle, 8, report, false);
         vTaskDelay(pdMS_TO_TICKS(20));
         memset(report, 0, 8);
-        esp_ble_gatts_send_indicate(s_gatts_if, conn_id_, s_hid_report_handle, 8, report, false);
+        esp_ble_gatts_send_indicate(s_gatts_if, s_conn_id, s_hid_report_handle, 8, report, false);
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
