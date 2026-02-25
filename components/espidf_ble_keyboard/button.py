7ad20dea259384f32f2b0d4bfc12c45f5c1e83e5
markus1234 <2220900+markusg1234@users.noreply.github.com>
2026-02-25 14:20:54 +1030

    Refactor BLE keyboard button implementation and consolidate button logic into a single file

--- components/espidf_ble_keyboard/button.py
+++ components/espidf_ble_keyboard/button.py
@@ -1,2 +1,27 @@
-# This file is intentionally empty.
-# Button platform is registered via espidf_ble_keyboard_button.py
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import button
+from esphome.const import CONF_ID
+from . import espidf_ble_keyboard_ns, EspidfBleKeyboard
+
+DEPENDENCIES = ["espidf_ble_keyboard"]
+
+EspidfBleKeyboardButton = espidf_ble_keyboard_ns.class_(
+    "EspidfBleKeyboardButton", button.Button
+)
+
+CONF_ACTION = "action"
+CONF_KEYBOARD_ID = "keyboard_id"
+
+CONFIG_SCHEMA = button.button_schema(EspidfBleKeyboardButton).extend({
+    cv.Required(CONF_KEYBOARD_ID): cv.use_id(EspidfBleKeyboard),
+    cv.Required(CONF_ACTION): cv.string,
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = await button.new_button(config)
+    await cg.register_component(var, config)
+
+    parent = await cg.get_variable(config[CONF_KEYBOARD_ID])
+    cg.add(var.set_parent(parent))
+    cg.add(var.set_action(config[CONF_ACTION]))
